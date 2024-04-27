5ce5cb94de73d2f79cab9d6fc6af64f3fdcab446
cuong20698 <vovietcuongcntt39b@gmail.com>
2024-04-27 15:25:30 +0700

    Add code navbar

---
+++ src/components/Section/navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import '../../styles/navbar.css'
+import avatartIcon from '../../assets/icon/avatar.svg'
+import badmintionIcon from '../../assets/icon/badminton-logo.png'
+import nemuIcon from '../../assets/icon/menu.svg'
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <ul>
+            <li>
+                <div className='nav-menu'>
+                    <img src={nemuIcon} alt='nemuIcon'></img>
+                </div>
+            </li>
+            <li>
+                <div className='nav-logo-container'>
+                    <div className='nav-logo'>
+                        <img src={badmintionIcon} alt='badmintionIcon'></img>
+                    </div>
+                    <div className='nav-title'>
+                        <p>badmintion</p>
+                        <hr></hr>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div className='nav-login-info'>
+                    <div className='nav-info-title'>cuong vo</div>
+                    <div className='nav-info-icon'>
+                        <img src={avatartIcon} alt='avatartIcon'></img>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Section/navbar'
+import '../styles/home.css'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
